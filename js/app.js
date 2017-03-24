e177f14d57b2100c9189adbf94cee26a59e1f013
tamerlan88111 <tamerlan88111@yandex.ru>
2017-03-24 17:47:12 +0300

    add yandex map

--- js/app.js
+++ js/app.js
@@ -7,10 +7,35 @@
   page_header.classList.remove('page-header--no-js');
   main_nav.classList.remove('main-nav--no-js');
 
+  var myMap;
+  function init() {
+    myMap = new ymaps.Map("map", {
+      center: [59.936365, 30.321668],
+      zoom: 16,
+      controls: []
+    });
+
+    myPlacemark = new ymaps.Placemark(
+      [59.936365, 30.321668],
+      {
+        hintContent: 'Мишка',
+        balloonContent: 'Интернет-магазин вязанных товаров'
+      },
+      {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/icon-map-pin.svg',
+        iconImageSize: [80, 80],
+        iconImageOffset: [-40, -70]
+      }
+    );
+    myMap.geoObjects.add(myPlacemark);
+  }
+
   document.addEventListener('DOMContentLoaded', function(){
     main_nav_button.addEventListener('click', function() {
       main_nav.classList.toggle('main-nav--open');
     });
-  })
 
+    ymaps.ready(init);
+  });
 }());
